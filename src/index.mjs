321cdd3bb8af901adf6e4efa6e2cf3e80ee3e4e0
yoshiiiiiiiiiii <112692664+yoshiiiiiiiiiii@users.noreply.github.com>
2023-11-01 09:38:36 +0900

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,14 @@
+/**
+ * const, let等の変数宣言
+ */
+var val1 = "var変数";
+console.log(val1);
+
+//var変数は上書き可能
+val1 = "var変数を上書き"
+console.log(val1);
+
+//var変数の再宣言可能
+var val1 = "変数の再宣言";
+console.log(val1);
+
